41d2a2d433a8128b8d9b02f5da5da4f8d761d182
vladimir <vladimir@inskysolutions.com>
2023-07-13 14:57:20 +0200

    added sucessful CRUD operations C and R

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
-from database import initialize_database
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from database import initialize_database, get_db
 from item import create_item, router as item_router
 from user import create_user, router as user_router
 from user_request import create_user_request, router as user_request_router
+from models import Item
 
 # App initialization.
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-# Initalize the database for table items.
-items_db = initialize_database()
-
+# Initialize the database.
+initialize_database()
 
 # Include the Item router from item.py
 app.include_router(item_router)
@@ -20,14 +24,41 @@ app.include_router(user_router)
 # Include the user_request router from user_request.py
 app.include_router(user_request_router)
 
-# Root route that needs to be modifed to represent the app.
+# Root route that needs to be modified to represent the app.
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
-@app.post("/items")
-def create_item_route(item):
-    return create_item(item)
+# Route for retreiving data from the database.
+@app.get("/items_list")
+def get_items(request: Request):
+    db = get_db()
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM items")
+    items = cursor.fetchall()
+
+    return templates.TemplateResponse("items_list.html", {"request": request, "items": items})
+
+# Route for inserting data with GET method which will give you interface for data insertion.
+@app.get("/create_item")
+def create_item_form(request: Request):
+    return templates.TemplateResponse("create_item.html", {"request": request})
+
+# Route for inserting data with POST method that will insert data into database.
+@app.post("/create_item")
+async def create_item_route(request: Request):
+    form_data = await request.form()
+    name = form_data.get("name")
+    price = form_data.get("price")
+    quantity = form_data.get("quantity")
+
+    # Insert the data.
+    item = Item(name=name, price=price, quantity=quantity)
+    create_item(item)
+
+    return templates.TemplateResponse("items_created.html", {"request": request, "item": item})
+    
 
 @app.post("/users")
 def create_user_route(user):
@@ -36,7 +67,3 @@ def create_user_route(user):
 @app.post("/user_requests")
 def create_user_request_route(user_request):
     return create_user_request(user_request)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
